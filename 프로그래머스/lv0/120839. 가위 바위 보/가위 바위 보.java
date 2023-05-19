3dc9e86916d98dd19ee027e7f90e7619893cfce5
이선아 <130373132+LeeSeona-dev@users.noreply.github.com>
2023-05-19 14:07:15 +0900

    [level 0] Title: 가위 바위 보, Time: 3.81 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        String[] str = rsp.split("");
+        String answer = "";
+        
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].equals("2")) {
+                answer += "0";
+            }
+            else if(str[i].equals("0")) {
+                answer += "5";
+            }
+            else if(str[i].equals("5")) {
+                answer += "2";
+            }
+        }return answer;
+    }
+}
